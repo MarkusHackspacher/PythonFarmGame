1cd2a074ce0aed0d35b3188bf3c53a1d584043c5
Markus Hackspacher <hackspacher@gmx.de>
2022-11-22 22:54:12 +0100

    merge

--- setup.py
+++ setup.py
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-from setuptools import find_packages, setup
-
-setup(
-    name="Python Farm Game",
-    version="0.6",
-=======
 from setuptools import setup
 
 setup(
     name="Python Farm Game",
-    version="0.5.2",
->>>>>>> 7eee31badfa52bf7eaad2d00ab227a27f9852364
+    version="0.6.2",
     description="Python farm game ",
     author="orneo1212",
     author_email="orneo1212@gmail.com",
@@ -20,8 +12,5 @@ setup(
             "main=farmlib.main:main",
         ],
     },
-<<<<<<< HEAD
     test_suite='tests',
-=======
->>>>>>> 7eee31badfa52bf7eaad2d00ab227a27f9852364
 )
